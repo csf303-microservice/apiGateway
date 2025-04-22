22699758bf19aba75387c9daeef961fc4cc9ed82
kuenzang24 <kuenzang24.dev@gmail.com>
2025-04-22 17:32:01 +0000

    env config

---
+++ src/test/java/bt/edu/gcit/apigateway/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.edu.gcit.apigateway.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
